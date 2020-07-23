1d815273fec8f1c4a2aecd23b2380c2880d14131
Guegan <leandre.guegan@linkt.fr>
2020-07-23 18:29:47 +0200

    Feat : Implements bar code style

--- src/services/BarCodeService.tsx
+++ src/services/BarCodeService.tsx
@@ -7,7 +7,7 @@ export const getBarCode = async (userId?: String) => {
     try {
         const barCodeUser = await axios.get(`${API_ROUTE}${ROUTE_GET_BARCODE}`,
             {
-                params: {idUser: '5f198fa50e9edcbf44f7f501'},
+                params: {idUser: '5f19b2819d62ce92803906d0'},
                 responseType: 'arraybuffer'
             });
 
